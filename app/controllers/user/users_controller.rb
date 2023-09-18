a6e61cf0160542dadcb7c53d28a9d3c183928977
tencoco <135337078+tencoco@users.noreply.github.com>
2023-09-18 13:26:01 +0000

    [Finish]PF

--- app/controllers/user/users_controller.rb
+++ app/controllers/user/users_controller.rb
@@ -9,11 +9,7 @@ class User::UsersController < ApplicationController
 
   def update
     @user = current_user
-    tags = Vision.get_image_data(user_params[:profile_image])
     if @user.update(user_params)
-      tags.each do |tag|
-        @user.tags.create(name: tag)
-      end
       redirect_to user_user_path(current_user)
     else
       redirect_to request.referer
